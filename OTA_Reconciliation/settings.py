3e9dea4c5832f49df3f205e615999228df6009b8
BadRat-in <rbmorena42@gmail.com>
2022-08-04 15:49:12 +0530

    pkg add gunicorn

--- OTA_Reconciliation/settings.py
+++ OTA_Reconciliation/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-r*!m1=p*%t0jqf_@a_kw5kbmwxa$nc)58y146_(e(o&h7n#i&b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ota-reconciliation.herokuapp.com']
 
 
 # Application definition
@@ -119,7 +120,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
