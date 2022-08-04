bd5f07e6734c3babeaad6ff14f433693874e690a
BadRat-in <rbmorena42@gmail.com>
2022-08-04 16:00:26 +0530

    pkg add gunicorn

--- OTA_Reconciliation/settings.py
+++ OTA_Reconciliation/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 WHITENOISE_USE_FINDERS = True
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
